b15c30cc676cfd40eebbf5178525cd5b3fcc59d0
Judy <judychue@pursuit.org>
2024-02-07 21:52:24 -0500

    first commit

---
+++ 1.5.24_Jose_Arrays.js
@@ -0,0 +1,140 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#examples
+
+/*
+Array 
+let nums = [1, 2, 3, 4, 5, 6]
+i have a list/collection of numbers
+typically we are dealing with a collection of a specific data type
+but that does not mean we can only store that data type
+
+- Array literal notation 
+
+let someVariable = []
+
+this is the syntax to create a new and empty array, We can also fill it with values
+
+
+let someSecondVariable = [1, 2, 3]
+
+- using constructor
+
+let someVariable = new Array(1, 2, 3)
+
+
+we use arrays to store data, specifically we do 
+not care about the individuality of the data, rather that we have a collection of it. 
+
+- Accessing values in the array
+if i wanted to take a look at the first value in the array, i would use its index value. 
+
+"I want the element at this position"
+
+but we have to remember that arrays are 0 based index, meaning that they dont start at 1, they start at 0
+
+if i want the first element, i use the 0 index. if i want the second element, i need to use 1 and so on and so forth
+
+if we try to access a value that does not exist, outside of the range of indexes, then we get undefined 
+
+
+
+- Removing and adding to an array
+.shift, .unshift, .pop, .push - are the methods that allow us to add and remove things to the front and back of the array. 
+
+manipulates the end of the array, or the last element
+.pop - remove from the end of the array 
+.push - adds an element to the end of the array
+
+
+manipulates the front of the array, or the first element
+.shift - remove the first element 
+.unshift - add to the front of the array
+
+we consider adding or removing from the start or end of the array to cost us no time. 
+*/
+//                  0  1  2  3  4
+// let someVariable = [1, 2, 3, 4, 5, 6]
+// let someVariable = new Array(1, 2, 3, 4)
+// console.log(someVariable[3])
+
+// continued from 1.5.24_Jose class
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+/* pseudo code
+  input: an array, containing numbers and strings containing a number
+  this is an opportunity to ask a follow up question
+  OUTPUT: returning a number, which is the total of adding all values in the array together
+*/
+
+// ASK JOSE for help with this
+// function sumOfNums(nums) {
+//   console.log(nums)
+
+// }
+
+// sumOfNums(789)
+// sumOfNums(nums1)
+
+// let answer =[]
+
+// let nums1 = [9, 3, '7', '3']
+// let nums2 = ['5', '0', 9, 3, 2, 1, '9', 6, 7]
+// let nums3 = ['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']
+
+// 1.4.24_LEETCODE:
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// var maxProfit = function(prices) {
+
+// };
+
+// submitted
+// var maxProfit = function(prices) {
+//     if(prices == null || prices.length <= 1) return 0;
+//     let minBuy = prices[0];
+//     let profit = 0;
+//     for(let i = 1; i < prices.length; i++) {
+//         minBuy = Math.min(minBuy, prices[i]);
+//         profit = Math.max(profit, prices[i] - minBuy);
+//     }
+//     return profit;
+// };
+
+// forward opt: https://www.youtube.com/watch?v=9ZMMNyHy3z4
+
+// [0,0,4,2,5,3]
+// [0,1,5,3,6,4]
+// [7,1,5,3,6,4]
+
+// function maxProfit (prices) {
+
+//     let buy = prices[0]; // 1
+//     prices[0] = 0;
+//     let profit = 0;
+
+//     for(let i = 1; i < prices.length; i++) {
+
+//         if(buy > prices[i]){
+//             buy = prices[i];
+//             prices[i] = 0;
+//         } else {
+//             profit = Math.max(prices[i]- buy, profit);
+//         }
+
+//         }
+//         return profit;
+
+// };
+
+// var maxProfit = function(prices) {
+//     let minBuyPrice=prices[0]
+//     let max=0
+//     for(let i=1; i <prices.length; i++) {
+//         const sellPrice=prices[i]
+//         const profit=sellPrice-minBuyPrice[i]
+//         max=Math.max(max,profit)
+//         minBuyPrice=Math.min(minBuyPrice, prices[i])
+//     }
+//     return max
+// };
---
+++ 1.7.24_pseudo_code.js
@@ -0,0 +1,121 @@
+// class activity: post pseudo code on class slack as a reply: 1.6.24
+
+/*
+Joses example pseudo code on R & S slack chat:
+input: x
+output: y
+
+how do we get there in english:
+something talking about the problem and how to go about it.
+
+translate to english ->
+something talking about solving the problem in code.
+
+
+My pseudo-breakdown #1: for 1.6.24 prob #1
+step 1: create a start and end point.
+input: x
+output: y
+
+step 2: how do we get from point A to B? 
+find the target value in an array of distinct integers, in the array of numbers and keeping the order, we need to return the index position of the target - if its found.
+
+If target is not found then, return the index number position where target number would be (inserted in order).
+*/
+
+// My pseudo-breakdown #2:
+// step 1: create a start and end point.
+// exhibit the numbers and and also the target - find the first part of question: which is find the index of the number provided in ex.
+// then find the second part of question: which is to find the index of a number in the ex given if its not in the ex.
+
+// step 2: how do we get from point A to B? 
+// part 1 - return the index position of the target number - if its found or in the array ex.
+
+// part 2 - If number is missing in ex array provided then return the target index position of that number (insert in order).
+
+// HERE is the code:
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If target is not found then, return the index number position where target number would be (if inserted in order).
+
+// Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+// Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+// Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Input: nums = [56, 67, 70, 77], target = 72 ; [56, 67, 70, 72, 77]
+// Input: nums = [56, 67, 70, 77], target = 72 => [56, 67, 70, 72, 77]
+Output: 3
+
+// code:
+// Jose explains part 2 of code- (which is) finding the index of a number that isn't in the given sorted array. In the else if statement: "At any given position, because the array is sorted, all numbers before the current position are smaller and all numbers after are bigger. so if we check and a number is now bigger than the target, then we have found where the target number belongs."
+
+function findTargetNums(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === target) {
+    // console.log(nums, target); 
+        // console.log(nums[i]); 
+        // return i;
+      } else if (nums[i] > target) {
+        // console.log(i);
+        return i;
+      }
+    }
+    // console.log(nums);
+    return nums.length;
+  }
+  
+  findTargetNums([56, 67, 70, 77], 72);
+  
+
+// function findNumbers(numbers) {
+//     console.log(numbers); 
+// }
+
+// findNumbers(1);
+
+
+
+// 1.7.24_code problem #2:
+// Given an array of integers, find if the array contains any duplicates.
+// Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+// Example 1:
+Input: [1,2,3,1]
+Output: true
+// Example 2:
+Input: [1,2,3,4]
+Output: false
+// Example 3:
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+/* My pseudo-breakdown #1: for 1.7.24 prob #2:
+
+input: an array of integers.
+output: if there are duplicates - return true and return false if no duplicates.
+Use Boolean
+
+- check if array contains integers, convert if it is not (parse float)?
+- create empty object (to store the available numbers)
+- loop through the integers to ck if there are or are not duplicates.
+- return true if intergers are more than 1.
+- return false if there is only 1 integer.
+*/
+
+
+// attempt at code: 
+// function containsDuplicates(nums) {
+// (create an empty object to hold the integers that are seen)
+//     for (let i = 0; i < nums.length; i++) {
+//         if ([nums[i] === ] ) {
+
+//         }
+
+//     }
+// }
